420f408067fd3b6b20f5bb3d6dcea638c73c61f7
yuriysteam <yuriysteam@icloud.com>
2016-05-17 01:15:40 +0200

    typedesign

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,6 @@
-$("#tab-description").click(function() {
+$(document).ready(function () {
+	
+	$("#tab-description").click(function() {
     $("#show-description").show();
     $("#show-process").hide();
     $("#tab-description").addClass("tab-active");
@@ -10,4 +12,23 @@ $("#tab-process").click(function() {
     $("#show-process").show();
     $("#tab-process").addClass("tab-active");
     $("#tab-description").removeClass("tab-active");
+});
+
+
+ $("#font-sample").css({
+        "color": $("#features-select option:selected").text(),
+        "font-size": parseInt($("#font-size").val())
+    });
+
+
+    $("#features-select").change(function () {
+        var $color = $("#features-select option:selected").text();
+        $("#f-showcase").css("color", $color);
+    });
+
+    $("#font-size").change(function () {
+        var $font_size = $("#font-size").val();
+        $("#f-showcase").css("font-size", parseInt($font_size));
+    });
+	
 });
