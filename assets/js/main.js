6ed05ad208a5d1848ba1279a2ba084f230f94365
yuriysteam <yuriysteam@icloud.com>
2016-05-17 21:46:35 +0200

    font

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,34 +1,31 @@
 $(document).ready(function () {
-	
-	$("#tab-description").click(function() {
-    $("#show-description").show();
-    $("#show-process").hide();
-    $("#tab-description").addClass("tab-active");
-    $("#tab-process").removeClass("tab-active"); 
-});
 
-$("#tab-process").click(function() {
-    $("#show-description").hide();
-    $("#show-process").show();
-    $("#tab-process").addClass("tab-active");
-    $("#tab-description").removeClass("tab-active");
-});
+	$("#tab-description").click(function () {
+		$("#show-description").show();
+		$("#show-process").hide();
+		$("#tab-description").addClass("tab-active");
+		$("#tab-process").removeClass("tab-active");
+	});
 
+	$("#tab-process").click(function () {
+		$("#show-description").hide();
+		$("#show-process").show();
+		$("#tab-process").addClass("tab-active");
+		$("#tab-description").removeClass("tab-active");
+	});
 
- $("#font-sample").css({
+	$("#font-sample").css({
         "color": $("#features-select option:selected").text(),
         "font-size": parseInt($("#font-size").val())
     });
 
-
     $("#features-select").change(function () {
         var $color = $("#features-select option:selected").text();
-        $("#f-showcase").css("color", $color);
+        $("#font-sample").css("color", $color);
     });
 
     $("#font-size").change(function () {
         var $font_size = $("#font-size").val();
-        $("#f-showcase").css("font-size", parseInt($font_size));
+        $("#font-sample").css("font-size", parseInt($font_size));
     });
-	
 });
