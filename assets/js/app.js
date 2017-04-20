fa24c49199b16b46a59d470bfef4ab449e7cf355
yuriysteam <yuriysteam@icloud.com>
2017-04-20 10:38:09 +0200

    chat 0.2

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,41 +1,41 @@
 var main = {
   id:"main",
   data: [
-    "Hello, my name is Yuriy. Welcome to our BA Final show",
-    "I am UI/UX designer",
-    "Shall I tell you more or you want to get in touch?",
-    "<div id='learn' class='chat-button button'>Learn more</div> <div id='getin' class='chat-button button'>Get in touch</div>"
+	"Hello, my name is Yuriy. Welcome to our BA Final show",
+	"I am UI/UX designer",
+	"Shall I tell you more or you want to get in touch?",
+	"<div id='learn' class='chat-button button'>Learn more</div> <div id='getin' class='chat-button button'>Get in touch</div>"
   ]
 }
 
 var weezzo_1 = {
   id: "weezzo_1",
   data: [
-    "Great, am pleased.",
-    "I am the graphic designer with the speciality in user interfaces",
-    "Or simply saying I am putting text on buttons and then arrange them on screen :)",
-    "For example, I've done a financial project with a lot of buttons called Weezoo",
-    "<img src='https://d13yacurqjgara.cloudfront.net/users/772985/screenshots/3204151/gf_1x.png' />",
-    "<div id='w_like' class='chat-button button'>Like it!</div> <div  id='w_no' class='chat-button button'>It's no fun</div>"
+	"Great, am pleased.",
+	"I am the graphic designer with the speciality in user interfaces",
+	"Or simply saying I am putting text on buttons and then arrange them on screen :)",
+	"For example, I've done a financial project with a lot of buttons called Weezoo",
+	"<img src='https://d13yacurqjgara.cloudfront.net/users/772985/screenshots/3204151/gf_1x.png' />",
+	"<div id='w_like' class='chat-button button'>Like it!</div> <div  id='w_no' class='chat-button button'>It's no fun</div>"
   ]
 }
 
 var card = {
   id: "weezzo_1",
   data: [
-    "I do appreciate your attention",
-    "Here is my digital business card",
-    "{card}",
-    "Do you want to talk about some specifics project or maybe see an example of my work? ",
-    "<div id='card_b' class='chat-button button'>Let's talk business</div> <div  id='card_w' class='chat-button button'>Show me your work</div>"
+	"I do appreciate your attention",
+	"Here is my digital business card",
+	"{card}",
+	"Do you want to talk about some specifics project or maybe see an example of my work? ",
+	"<div id='card_b' class='chat-button button'>Let's talk business</div> <div  id='card_w' class='chat-button button'>Show me your work</div>"
   ]
 }
 
 var car_ui = {
   id: "car_ui",
   data: [
-    "Great, we speak same language!",
-    "Ok. One more",
+	"Great, we speak same language!",
+	"Ok. One more",
 	"For the school final show, I tried to imagine how car UI of the future will look like",
 	  "<div id='car_y' class='chat-button button'>Looks cool</div> <div  id='car_n' class='chat-button button'>Nah! :| </div>"
   ]
@@ -44,8 +44,8 @@ var car_ui = {
 var taste = {
   id: "taste",
   data: [
-    "You have a great taste I shall admit :)",
-    "Would you want to talk about design or look at other works?",
+	"You have a great taste I shall admit :)",
+	"Would you want to talk about design or look at other works?",
 	  "<div id='taste_y' class='chat-button button'>Let's talk!</div> <div  id='brows' class='chat-button button'>Browse more</div>"
   ]
 }
@@ -53,8 +53,8 @@ var taste = {
 var prg_1 = {
   id: "prg_1",
   data: [
-    "I  will be happy to discuss your work or new design trends",
-    "Are you in Prague maybe?",
+	"I  will be happy to discuss your work or new design trends",
+	"Are you in Prague maybe?",
 	  "<div id='prg_y' class='chat-button button'>Yes!</div> <div  id='prg_n' class='chat-button button'>Nope :P</div>"
   ]
 }
@@ -62,7 +62,7 @@ var prg_1 = {
 var coffee = {
   id: "coffee",
   data: [
-    "Great. Do you like coffee?",
+	"Great. Do you like coffee?",
 	  "<div id='coffee_y' class='chat-button button'>Yes</div> <div  id='coffee_n' class='chat-button button'>No</div>"
   ]
 }
@@ -70,7 +70,7 @@ var coffee = {
 var drink = {
   id: "drink",
   data: [
-    "Okey. Drop me a line and we can meet.",
+	"Okey. Drop me a line and we can meet.",
 	  "Looking forward to talking to you",
 	  "{Business card with details}",
 	  "{Gif with cat drinking hot something}"
@@ -80,7 +80,7 @@ var drink = {
 var biz = {
   id: "biz",
   data: [
-      "Superb!",
+	  "Superb!",
 	  "Like your attitude. I will ask you to tell me about your project, so I can prepare",
 	  "What do you need? Logo, interface or typeface? Tell me in the form",
 	  "{form for this}",
@@ -88,14 +88,14 @@ var biz = {
 	  "{form for e-mail}",
 	  "Gotcha. All noted, saved and I am already thinking about the solution.",
 	  "{gif with a cat sitting and working}"
-	  
+
   ]
 }
 
 var weezzo_2 = {
   id: "weezzo_2",
   data: [
-    "Thank you for your interest",
+	"Thank you for your interest",
 	  "Recently, I've done a great financial project called Weezzo",
 	  "{Project img here}",
 	  "<div id='weezzo__2_y' class='chat-button button'>Like it!</div> <div  id='weezzo__2_n' class='chat-button button'>Maybe something else?</div>"
@@ -105,7 +105,7 @@ var weezzo_2 = {
 var brand_t = {
   id: "brand_t",
   data: [
-    "Hm, what about brand identity?",
+	"Hm, what about brand identity?",
 	  "Or maybe type design?",
 	  "<div id='brand_1' class='chat-button button'>Brands</div> <div  id='type_1' class='chat-button button'>Typedesign</div>"
   ]
@@ -115,7 +115,7 @@ var brand_t = {
 var reetimee = {
   id: "reetimee",
   data: [
-    "So brands it is",
+	"So brands it is",
 	  "Here is an identity I did for a luxury fashion brand",
 	  "I like when a client is fully involved and this project is the great example",
 	  "img",
@@ -126,7 +126,7 @@ var reetimee = {
 var negative = {
   id: "negative",
   data: [
-    "I guess you have a different taste",
+	"I guess you have a different taste",
 	  "And this is cool.. ",
 	  "Tell me, what do you like? Drop a line or share link",
 	  "I am interested what you are working on",
@@ -134,28 +134,28 @@ var negative = {
 	  "Thank you for this",
 	  "I will be gone. For now",
 	  "{ninja gif}"
-	  
+
   ]
 }
 
 var prg_2 = {
   id: "prg_2",
   data: [
-     "Aha, so you live abroad",
+	 "Aha, so you live abroad",
 	  "Always wanted to meet awesome designers from foreign countries.",
 	  "Here is my card",
 	  "{card}",
 	  "Drop me a line. I like travel places",
 	  "Looking forward to talking to you.",
 	  "{Gif with cat drinking hot something}"
-	  
+
   ]
 }
 
 var tea = {
   id: "tea",
   data: [
-    "Maybe a cup of tea?",
+	"Maybe a cup of tea?",
 	  "<div id='tea_y' class='chat-button button'>Why not?</div> <div  id='tea_n' class='chat-button button'>No</div>"
   ]
 }
@@ -163,21 +163,21 @@ var tea = {
 var tea_f = {
   id: "tea_f",
   data: [
-     "Hm.",
+	 "Hm.",
 	  "I really underestimated your beverage choices.",
 	  "Well, at least we are certain about design.",
 	  "Drop me a line and let's meet.",
 	  "{card}",
 	  "Looking forward to talking to you.",
 	  "{Gif with cat drinking hot something}"
-	  
+
   ]
 }
 
 var typog = {
   id: "typog",
   data: [
-    "So typography it is",
+	"So typography it is",
 	  "I did two typefaces",
 	  "img",
 	  "<div id='typog_y' class='chat-button button'>This is my type of work!</div> <div  id='typog_n' class='chat-button button'>Nah</div>"
@@ -187,7 +187,7 @@ var typog = {
 var fotograf = {
   id: "fotograf",
   data: [
-    "Great, great",
+	"Great, great",
 	  "I like branding too, because it express the whole idea or message of the company or person.",
 	  "Like I did for Czech photography magazine",
 	  "{img}",
@@ -198,7 +198,7 @@ var fotograf = {
 var portfolio = {
   id: "portfolio",
   data: [
-    "Ok",
+	"Ok",
 	  "What if…",
 	  "Maybe something from my portfolio will be interesting for you?",
 	  "{several images}",
@@ -209,7 +209,7 @@ var portfolio = {
 var portfolio_link = {
   id: "portfolio_link",
   data: [
-     "Link to portfolio"
+	 "Link to portfolio"
   ]
 }
 
@@ -218,151 +218,200 @@ $(function() {
   renderChatMessages(main);
 
   $(document).on('click', '.chat-button', function(el){
-    var buttonName = el.currentTarget.id;
-    var buttonContext = $(el.currentTarget.parentElement).data('id');
-    var btnPosition = $(el.currentTarget).position();
+	var buttonName = el.currentTarget.id;
+	var buttonContext = $(el.currentTarget.parentElement).data('id');
+	var btnPosition = $(el.currentTarget).position();
 
-    hideButtons(buttonContext);
-    showActiveButton(buttonName, btnPosition);
+	hideButtons(buttonContext);
+	showActiveButton(buttonName, btnPosition);
 
-    switch(buttonName) {
-      case 'learn':
-          clickChatButton(weezzo_1);
-          break;
+	switch(buttonName) {
+	  case 'learn':
+		  clickChatButton(weezzo_1);
+		  break;
 	case 'getin':
-          clickChatButton(card);
-          break;
+		  clickChatButton(card);
+		  break;
 	case 'card_b':
-          clickChatButton(biz);
-          break;
-      case 'w_like':
-          clickChatButton(car_ui);
+		  clickChatButton(biz);
+		  break;
+	  case 'w_like':
+		  clickChatButton(car_ui);
 		  break;
 	  case 'car_y':
-          clickChatButton(taste);
+		  clickChatButton(taste);
 		  break;
 		case 'taste_y':
-          clickChatButton(prg_1);
+		  clickChatButton(prg_1);
 		  break;
-      case 'prg_y':
-          clickChatButton(coffee);
+	  case 'prg_y':
+		  clickChatButton(coffee);
 		  break;
 		case 'coffee_y':
-          clickChatButton(drink);
+		  clickChatButton(drink);
 		  break;
 		case 'card_w':
-          clickChatButton(weezzo_2);
+		  clickChatButton(weezzo_2);
 		  break;
 		case 'weezzo__2_y':
-          clickChatButton(car_ui);
+		  clickChatButton(car_ui);
 		  break;
 		case 'w_no':
-          clickChatButton(brand_t);
+		  clickChatButton(brand_t);
 		  break;
 		case 'brand_1':
-          clickChatButton(reetimee);
+		  clickChatButton(reetimee);
 		  break;
 			case 'reetimee_y':
-          clickChatButton(fotograf);
+		  clickChatButton(fotograf);
 		  break;
 		case 'reetimee_n':
-          clickChatButton(negative);
+		  clickChatButton(negative);
 		  break;
 		case 'fotograf_y':
-          clickChatButton(taste);
+		  clickChatButton(taste);
 		  break;
 			case 'brows':
-          clickChatButton(portfolio_link);
+		  clickChatButton(portfolio_link);
 		  break;
 		case 'fotograf_n':
-          clickChatButton(negative);
+		  clickChatButton(negative);
 		  break;
 			case 'weezzo__2_n':
-          clickChatButton(brand_t);
+		  clickChatButton(brand_t);
 		  break;
 			case 'prg_n':
-          clickChatButton(prg_2);
+		  clickChatButton(prg_2);
 		  break;
 				case 'tea_n':
-          clickChatButton(tea_f);
+		  clickChatButton(tea_f);
 		  break;
 				case 'tea_y':
-          clickChatButton(drink);
+		  clickChatButton(drink);
 		  break;
 				case 'coffee_n':
-          clickChatButton(tea);
+		  clickChatButton(tea);
 		  break;
 			case 'type_1':
-          clickChatButton(typog);
+		  clickChatButton(typog);
 		  break;
 			case 'typog_y':
-          clickChatButton(prg_1);
+		  clickChatButton(prg_1);
 		  break;
 			case 'fotograf_y':
-          clickChatButton(taste);
-		  break;	
+		  clickChatButton(taste);
+		  break;
 		case 'fotograf_n':
-          clickChatButton(negative);
+		  clickChatButton(negative);
 		  break;
 		case 'car_n':
-          clickChatButton(portfolio);
+		  clickChatButton(portfolio);
 		  break;
 			case 'portfolio_y':
-          clickChatButton(taste);
+		  clickChatButton(taste);
 		  break;
 		case 'portfolio_n':
-          clickChatButton(negative);
+		  clickChatButton(negative);
 		  break;
-    }
+	}
   })
 
 });
 
 function renderChatMessages(msgArray) {
-  for (var i = 0; i < msgArray.data.length; i++) {
-    var listItem = '';
-    var item = msgArray.data[i];
-    var n = item.includes("class='chat-button button'");
-    if(n){
-      listItem = '<div class="list-item hideLi '+ msgArray.id +'"><div class="buttons-area" data-id="'+ msgArray.id +'">' + msgArray.data[i] + '</div></div>';
-    } else {
-      listItem = '<div class="list-item hideLi '+ msgArray.id +'"  id="'+ msgArray.id + i +'"><div class="message" data-id="'+ msgArray.id +'">' + msgArray.data[i] + '</div></div>';
-    }
-    $(".chat-wrapper").append(listItem);
+	var div, chdiv;
+	for (var i = 0; i < msgArray.data.length; i++)
+	{
+		var listItem = '';
+		var item = msgArray.data[i];
+		var n = item.includes("class='chat-button button'");
+		div = document.createElement("div");
+		div.className = 'list-item '+msgArray.id;
+		chdiv = document.createElement("div");
 
-  }
+		if (n)
+		{
+			div.className += " is-buttons";
+			chdiv.className = "buttons-area";
+			chdiv.id = msgArray.id;
+			chdiv.innerHTML = msgArray.data[i];
+		}
+		else
+		{
+			div.className += " is-message";
+			div.id = msgArray.id+i;
+			chdiv.className = "message";
+			chdiv.id = msgArray.id;
+			chdiv.innerHTML = "<span class='chat-dots'><span class='dot'></span></span>";
+		}
+		div.appendChild(chdiv);
+		$(".chat-wrapper").append(div);
 
-   addAnimation(msgArray.id);
+		(function(div, content, i){
+			setTimeout(function(){
+				showAnimate(div, content);
+			}, i * 2500);
+		})(div, (!n ? msgArray.data[i] : null), i);
+	}
 
    var scrollToEl = '#' + msgArray.id + '0';
    $('html, body').animate({
-         scrollTop: $(scrollToEl).offset().top-180
-     }, 900);
+		 scrollTop: $(scrollToEl).offset().top-180
+	 }, 900);
 }
 
 function clickChatButton(branch) {
-    renderChatMessages(branch);
+	renderChatMessages(branch);
 }
 
-function addAnimation(arrayId) {
-  var listItem = '.list-item.' + arrayId;
-  $(listItem).each(function(i) {
-   $(this).delay((++i) * 800).fadeTo(2000, 1).addClass('animation');
- })
+function showAnimate(div, text) {
+
+	setTimeout(function(){
+		$(div).addClass("is-visible");
+
+		if (!text)
+			return true;
+
+		setTimeout(function(){
+			$(div).find(".chat-dots").addClass("is-hidden");
+			setTimeout(function(){
+				var m = div.children[0];
+				var w = $(m).width();
+				var h = $(m).height();
+				m.innerHTML = text;
+				var w2 = $(m).width();
+				var h2 = $(m).height();
+				$(m).width(w);
+				$(m).height(h);
+				setTimeout(function(){
+					$(m).addClass("is-animate");
+
+					setTimeout(function(){
+						$(m).width(w2);
+						$(m).height(h2);
+						setTimeout(function(){
+							$(m).removeAttr("style");
+							$(m).addClass("is-visible");
+						}, 750);
+					}, 1);
+
+				}, 1);
+			}, 150);
+		}, 2500);
+
+	}, 750);
 }
 
 function hideButtons(buttonContext) {
-  $(".buttons-area[data-id='"+buttonContext+"'] .chat-button").hide().removeClass('chat-button');
+	$(".buttons-area[data-id='"+buttonContext+"'] .chat-button").hide().removeClass('chat-button');
 }
 
 function showActiveButton(buttonName, btnPosition) {
-  var $button = $('#'+ buttonName);
-  $button.show();
-  $button.css({
-    top: btnPosition.top,
-    left: btnPosition.left
-  });
-  $button.animate({
-    left: 220
-  }, 1500 );
+	var $button = $('#'+buttonName);
+	$button.css({right: $button.parent().width()-($button.offset().left+$button.outerWidth()), position: 'absolute'});
+	$button.parent().find(".button:not(#"+buttonName+")").hide();
+	setTimeout(function(){
+		$button.addClass("is-chosen");
+		$button.removeAttr("style");
+	}, 1);
 }
