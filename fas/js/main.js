438ecff84fe762460a4ef957ee8c1bb99cdc9849
yuriysteam <yuriysteam@icloud.com>
2019-04-16 20:50:33 +0200

    phone

--- fas/js/main.js
+++
@@ -1,5 +0,0 @@
-var par = document.getElementsByClassName('section-collapse')
-var el = document.getElementsByClassName('button-collapse')
-el[0].addEventListener("click", function(e){
-    par[0].classList.toggle("section-collaps-bg")
-})
