98e64b9a55d62b4b2db9962f045a348adf90b058
Yuriy Shiryaev <yuriysteam@icloud.com>
2020-05-18 22:05:08 +0200

    audio in 2

--- vf/assets/js/main.js
+++ vf/assets/js/main.js
@@ -1,16 +1,19 @@
-var mic;
+let mic;
 
  function setup(){
-  createCanvas(200, 200);
+  let cnv = createCanvas(100, 100);
+  cnv.mousePressed(userStartAudio);
+  textAlign(CENTER);
   mic = new p5.AudioIn();
   mic.start();
 }
 
 function draw(){
   background(0);
+  fill(255);
+  text('tap to start', width/2, 20);
 
   micLevel = mic.getLevel();
-  ellipse(100, 100, micLevel*500, micLevel*500);
-  
-  console.log(micLevel);
+  let y = height - micLevel * height;
+  ellipse(width/2, y, 10, 10);
 }
