ea3572071fa12586deb5e1d547710a0f4daaa836
Yuriy Shiryaev <yuriysteam@icloud.com>
2020-05-18 21:36:39 +0200

    vol submite

--- vf/assets/js/main.js
+++ vf/assets/js/main.js
@@ -1,29 +1,12 @@
-var pageMain = document.querySelector('main');
-var wghtVal = '"wght" 140';
-var spacVal = '"SPAC" 80';
-var heigVal = '"HEIG" 400';
-var asceVal = '"ASCE" 600';
-var descVal = '"DESC" 600';
-var diacVal = '"DIAC" 0';
-var monoVal = '"MONO" 0';
-var curvVal = '"CURV" 0';
-setStyles();
+var mic;
 
-function setStyles() {
-  pageMain.setAttribute('style','font-variation-settings: ' + wghtVal + ', ' + spacVal + ', ' + heigVal + ', ' + asceVal + ', ' + descVal + ', ' + diacVal + ', ' + monoVal + ', ' + curvVal + ';');
+function setup () {
+    mic = new p5.AudioIn();
+    mic.start();
+    
 }
 
-function changeAxis(eID,num,axis,fldReset) {
-  document.querySelector('#' + eID + 'Val').value = num;
-  document.querySelector('#' + eID).setAttribute('aria-valuenow',num);
-  tmpVal = '"' + axis + '" ' + num;
- 
-   
-  new Function(axis.toLowerCase() +"Val  = tmpVal")();
-  setStyles();
-  if (fldReset == '1') {
-    document.querySelector('#' + eID).value = num;
-    document.getElementById(eID).focus();
-  }
+function draw() {
+    var vol = mic.getLevel();
+    console.log(vol);
 }
-
