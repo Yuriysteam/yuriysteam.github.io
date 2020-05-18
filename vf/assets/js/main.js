918e84596ce48db1133de940befd479d278565d7
Yuriy Shiryaev <yuriysteam@icloud.com>
2020-05-18 22:17:36 +0200

    audio 5

--- vf/assets/js/main.js
+++ vf/assets/js/main.js
@@ -1,18 +1,29 @@
-let mic;
+var mic
+var vol = 0
 
- function setup(){
-  let cnv = createCanvas(500, 500);
-  cnv.mousePressed(userStartAudio);
-  textAlign(CENTER);
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  // Create an Audio input
   mic = new p5.AudioIn();
+     // start the Audio Input.
+  // By default, it does not .connect() (to the computer speakers)
   mic.start();
 }
 
-function draw(){
-  background(0);
-  fill(255);
-  text('tap to start', width/2, 20);
+function draw() {
+  background(200);
 
-  micLevel = mic.getLevel();
-  ellipse(250, 250, 400, micLevel*300);
+  // Get the overall volume (between 0 and 1.0)
+   var vol = mic.getLevel();
+  
+  fill(127);
+  stroke(0);
+  
+  // Draw an ellipse with height based on volume
+  var h = map(vol, 0, 0.5, height, 0);
+  ellipse(width/2, h - 25, 50, 50);
+}
+
+function touchStarted() {
+  getAudioContext().resume()
 }
