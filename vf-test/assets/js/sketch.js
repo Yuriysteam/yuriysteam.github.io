3e1ce1799416f691373133f729c3ae71b9f328af
Yuriy Shiryaev <yuriysteam@icloud.com>
2020-06-01 11:50:12 +0200

    upd project

--- vf-test/assets/js/sketch.js
+++ vf-test/assets/js/sketch.js
@@ -25,5 +25,5 @@ function draw() {
   micLevel = mic.getLevel()
   // weightControl = sin(frameCount/1.0)*100;
   div.style['font-variation-settings'] = `"wght" ${micLevel * 4000}, "wdth" ${micLevel * 5000}`
-  console.log('micLevel', micLevel)
+ // console.log('micLevel', micLevel)
 }
